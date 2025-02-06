2aa7de27e9b3078472ee3c9442f474b0983da623
AngHHdz <135458656+AngHHdz@users.noreply.github.com>
2025-02-06 16:11:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -45,11 +45,11 @@ if pdf_file and excel_file:
 
                     # Insertar texto en vertical al lado derecho de la coincidencia
                     page.insert_textbox(
-                        fitz.Rect(x + offset_x, y, x + offset_x + 20, y + 100),
+                        fitz.Rect(x + offset_x, y, x + offset_x + 120, y + 100),
                         texto_a_insertar,
                         fontsize=10,
                         color=(0, 0, 0, 0.188),
-                        rotate=180  # Rotación vertical
+                        rotate=-90  # Rotación vertical
                     )
 
     # Guardar el PDF modificado
